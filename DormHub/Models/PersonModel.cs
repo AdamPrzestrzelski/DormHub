641552c72907099b923fde3980dac9ebfe04d90c
milosz-zatowka <miloszek696@gmail.com>
2026-03-17 19:48:21 +0100

    Added PersonModel

--- DormHub/Models/PersonModel.cs
+++ DormHub/Models/PersonModel.cs
@@ -1,6 +1,19 @@
-﻿namespace DormHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DormHub.Models
 {
     public class Person
     {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
     }
 }
--- DormHub/Models/RoomModel.cs
+++ DormHub/Models/RoomModel.cs
@@ -1,7 +1,10 @@
-﻿namespace DormHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DormHub.Models
 {
     public class RoomModel
     {
+        [Key]
         public int Id { get; set; }
         public int RoomNumber { get; set; }
         public int BuildingId { get; set; }
