9f17966f8eb064fffe8b472aa78e1bf7cc1c80c1
Matryx <milewskihubertaha@gmail.com>
2026-03-17 18:32:57 +0100

    Initial commit

---
+++ DormHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DormHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
