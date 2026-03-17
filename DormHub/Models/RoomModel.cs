d2462e6a7498dd000d3f7776d4f529e36e21d328
milosz-zatowka <miloszek696@gmail.com>
2026-03-17 19:41:08 +0100

    name change

--- DormHub/Models/RoomModel.cs
+++ DormHub/Models/RoomModel.cs
@@ -6,7 +6,7 @@
         public int RoomNumber { get; set; }
         public int BuildingId { get; set; }
         public int Capacity { get; set; }
-        public bool isPenthouse { get; set; }
+        public bool isDeluxe { get; set; }
 
     }
 }
