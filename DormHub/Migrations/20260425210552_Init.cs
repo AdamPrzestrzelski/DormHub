9eb5988a25c89c59daf430657f72e42abe88dde2
Adam <aprzestrzelski@wp.pl>
2026-04-27 01:59:34 +0200

    Fix Migrations

--- DormHub/Migrations/20260425210552_Init.cs
+++ DormHub/Migrations/20260425210552_Init.cs
@@ -11,35 +11,6 @@ namespace DormHub.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "MoveOutDate",
-                table: "Persons",
-                newName: "MoveoutDate");
-
-            migrationBuilder.RenameColumn(
-                name: "MoveInDate",
-                table: "Persons",
-                newName: "MoveinDate");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "MoveoutDate",
-                table: "Persons",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime2",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "MoveinDate",
-                table: "Persons",
-                type: "nvarchar(max)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime2",
-                oldNullable: true);
 
             migrationBuilder.AddColumn<bool>(
                 name: "IsActive",
@@ -47,18 +18,6 @@ namespace DormHub.Migrations
                 type: "bit",
                 nullable: false,
                 defaultValue: false);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "MoveInDate",
-                table: "Persons",
-                type: "datetime2",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "MoveOutDate",
-                table: "Persons",
-                type: "datetime2",
-                nullable: true);
 
             migrationBuilder.AddColumn<string>(
                 name: "PasswordHash",
